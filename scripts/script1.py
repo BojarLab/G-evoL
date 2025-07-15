dbbd64e16d3c92cbae6ed8a65991cc8d68433a73
zeyak <zeynep.hitay.akdeniz@gmail.com>
2025-07-15 15:06:13 +0200

    species tree is removed

--- scripts/script1.py
+++ scripts/script1.py
@@ -4,28 +4,6 @@ from glycowork.glycan_data.loader import df_glycan, df_species
 from ete3 import NCBITaxa
 ncbi = NCBITaxa()
 
-sp_list = (
-    df_species["Species"]
-    .drop_duplicates()
-    .str.strip()
-    .str.replace("_", " ")
-    .tolist()
-)
-
-taxids = []
-count = 0
-for sp in sp_list:
-    name2taxid = ncbi.get_name_translator([sp])
-    if sp in name2taxid:
-        taxids.append(name2taxid[sp][0])
-    else:
-        print(f"Warning: Taxid not found for {sp}")
-        count += 1
-tree = ncbi.get_topology(taxids)
-print(tree.get_ascii(show_internal=True))
-print(f"Taxid not found: {count}")
-tree.write(outfile="output/2918_species_tree.nw")
-
 unique_glycans = list(set(df_species["glycan"]))
 annots = annotate_dataset(unique_glycans, feature_set=["known", "exhaustive"])
 annots = annots.reset_index().rename(columns={"index": "glycan"})
